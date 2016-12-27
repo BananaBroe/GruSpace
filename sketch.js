636eabd6c57dbd5bdc0292a5693466c346c9a4c6
BananaBroe <103.gruber@gmail.com>
2016-12-27 16:36:59 +0100

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,13 @@
+function setup() 
+{
+   createCanvas(600,600);
+   background(200);
+}
+
+function draw() 
+{
+   fill(255,0,0);
+   rect(300,300,50,50);
+   rect(300,300,-50,-50);
+   ellipse(300,300,100,100);  
+}
